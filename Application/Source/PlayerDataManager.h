cf9d30e4a34b0186008b130eb55513aa0b3115c7
Hui Sheng <160212S@M322G804.sidm.student.local>
2017-02-16 13:04:22 +0800

    Initial PlayerStat implementation (might be buggy)

--- Application/Source/PlayerDataManager.h
+++ Application/Source/PlayerDataManager.h
@@ -1,6 +1,8 @@
 #ifndef PlayerDataManager_H
 #define PlayerDataManager_H
 
+#include <map>
+
 struct PlayerData {
 	int currency;
 
@@ -49,12 +51,15 @@ struct PlayerData {
 
 struct PlayerStat {
 
-	int health;
-	int shield;
-
-	int base_damage;
 	int base_speed;
+	int base_damage;
 	int base_shield;
+
+	int initial_bullet_damage;
+	int initial_bullet_speed;
+	int initial_bullet_cooldown;
+	int initial_shield_capacity;
+	int initial_shield_recoveryRate;
 
 	int current_bullet_damage;
 	int current_bullet_speed;
@@ -62,11 +67,27 @@ struct PlayerStat {
 	int current_shield_capacity;
 	int current_shield_recoveryRate;
 
-	int initial_bullet_damage;
-	int initial_bullet_speed;
-	int initial_bullet_cooldown;
-	int initial_shield_capacity;
-	int initial_shield_recoveryRate;
+	int current_fighter;
+	int current_health;
+	int current_shield;
+
+
+};
+
+const int Ship_Stats[12] {
+
+	// Format: Speed-Damage-Shield
+	// Default: 
+	1, 1, 1,
+
+		// SF-1
+		2, 1, 0,
+
+		// DF-6
+		1, 2, 0,
+
+		// A-10
+		0, 1, 2
 
 };
 
@@ -77,7 +98,8 @@ public:
 	static PlayerDataManager* getInstance();
 
 	// Flushes the PlayerData file and write pData into it
-	void SaveData();
+	// Args 0: Current Space Fighter that the player is using
+	void SaveData(int currentFighter);
 
 	// Load the PlayerData file and store it into a vector
 	void LoadPlayerData();
@@ -87,17 +109,35 @@ public:
 
 	// Contains information about player purchased Upgrades
 	PlayerData* getPlayerData() { return &pData; };
-	
+
 	// Contains information about player upgrades purchased
 	PlayerStat* getPlayerStats() { return &pStatData; };
 
 private:
 	PlayerDataManager();
 	~PlayerDataManager() { delete _instance; };
+
 	PlayerData pData;
 	PlayerStat pStatData;
+	
+	int current_bullet_damage;
+	int current_bullet_speed;
+	int current_bullet_cooldown;
+	int current_shield_capacity;
+	int current_shield_recoveryRate;
 
+	const int base_bullet_speed = 1;
+	const int base_bullet_cooldown = 1;
+	const int base_shield_recoveryrate = 1;
 
+	const int multiplier_bullet_damage = 1;
+	const int multiplier_bullet_speed = 1;
+	const int multiplier_bullet_cooldown = 2;
+	const int multiplier_shield_capacity = 1;
+	const int multiplier_shield_recoveryRate = 2;
+
+	const int default_health = 100;
+	const int default_shield = 100;
 
 };
 #endif

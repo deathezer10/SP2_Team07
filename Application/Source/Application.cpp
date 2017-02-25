310f660cd722496512033ce2596d74f06badd9ba
HUISHENG-PC\death <deathezer10@live.com.sg>
2017-02-26 00:13:24 +0800

    1. Added Fullscreen mode (Hotkey: F5) 2. Removed some redudant codes 3. Updated GLFW & GLEW to the latest 32-bit version

--- Application/Source/Application.cpp
+++ Application/Source/Application.cpp
@@ -15,106 +15,134 @@
 #include "SceneMainMenu.h"
 #include "SceneShop.h"
 
+
 GLFWwindow* m_window;
 
+bool Application::_isFullScreen = false;
+bool Application::_isValid = true;
+
+int Application::_windowWidth = 1024;
+int Application::_windowHeight = 768;
+
+
 //Define an error callback
-static void error_callback(int error, const char* description)
-{
+static void error_callback(int error, const char* description) {
 	fputs(description, stderr);
 	_fgetchar();
 }
 
-//Define the key input callback
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	/*if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);*/
-	//in case needed 
-}
-
-bool Application::IsKeyPressed(unsigned short key)
-{
-	return ((GetAsyncKeyState(key) & 0x8001) != 0);
-}
-
-Application::Application()
-{
-}
-
-Application::~Application()
-{
-}
-
-void resize_callback(GLFWwindow* window, int w, int h)
-{
+//Define window resizing callback
+void resize_callback(GLFWwindow* window, int w, int h) {
 	glViewport(0, 0, w, h); //update opengl the new window size
 }
 
-void Application::Init()
-{
+//Define the key input callback
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_F5 && action == GLFW_RELEASE) {
+		Application::ToggleFullscreen();
+	}
+}
+
+void Application::ToggleFullscreen() {
+
+	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	if (_isFullScreen == false) { // Switch to Borderless Fullscreen
+		glfwSetWindowMonitor(m_window, glfwGetPrimaryMonitor(), 0, 0, mode->width, mode->height, mode->refreshRate);
+	}
+	else { // Switch to Windowed
+		glfwSetWindowMonitor(m_window, NULL, mode->width / 5, mode->height / 5, _windowWidth, _windowHeight, GL_DONT_CARE);
+	}
+
+	_isValid = false; // Window was resized
+
+	_isFullScreen = !_isFullScreen;
+
+}
+
+bool Application::IsKeyPressed(unsigned short key) {
+	return ((GetAsyncKeyState(key) & 0x8001) != 0);
+}
+
+Application::Application() {
+}
+
+Application::~Application() {
+}
+
+void Application::Init() {
+
 	//Set the error callback
 	glfwSetErrorCallback(error_callback);
 
 	//Initialize GLFW
-	if (!glfwInit())
-	{
+	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
 	}
+
+	//Current monitor's video mode
+	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
 	//Set the GLFW window creation hints - these are optional
 	glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+
+	//Set the Video mode to same as the current one
+	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
 
 	//Create a window and create its OpenGL context
 	m_window = glfwCreateWindow(_windowWidth, _windowHeight, "Space Fighter 27", NULL, NULL);
-	glfwSetWindowUserPointer(m_window, nullptr); // To retrieve the window handle in other classes
-	glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	//Center the window
+	glfwSetWindowPos(m_window, mode->width / 5, mode->height / 5);
 
 	//If the window couldn't be created
-	if (!m_window)
-	{
+	if (!m_window) {
 		fprintf(stderr, "Failed to open GLFW window.\n");
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
 
-	// Resize Window callback
-	glfwSetWindowSizeCallback(m_window, resize_callback);
+	glfwSetWindowUserPointer(m_window, nullptr); // To retrieve the window handle in other classes
+	glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // Disable cursor on start
 
 	//This function makes the context of the specified window current on the calling thread. 
 	glfwMakeContextCurrent(m_window);
 
+	//Resize Window callback
+	glfwSetWindowSizeCallback(m_window, resize_callback);
+
 	//Sets the key callback
-	//glfwSetKeyCallback(m_window, key_callback);
+	glfwSetKeyCallback(m_window, key_callback);
 
 	glewExperimental = true; // Needed for core profile
+
 	//Initialize GLEW
 	GLenum err = glewInit();
 
 	//If GLEW hasn't initialized
-	if (err != GLEW_OK)
-	{
+	if (err != GLEW_OK) {
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
 		//return -1;
 	}
 }
 
-void Application::Run()
-{
+void Application::Run() {
 	SceneManager* scene_manager = SceneManager::getInstance();
 
 	scene_manager->changeScene(new SceneMainMenu());
 	scene_manager->Update();
-
 }
 
-void Application::Exit()
-{
+void Application::Exit() {
 	//Close OpenGL window and terminate GLFW
 	glfwDestroyWindow(m_window);
+
 	//Finalize and clean up GLFW
 	glfwTerminate();
 }
--- Application/Source/Application.h
+++ Application/Source/Application.h
@@ -5,8 +5,8 @@
 #include "timer.h"
 
 // Must be included before GLFW/GLEW or it willl cause "macro redefinition"
-class Application
-{
+class Application {
+
 public:
 	Application();
 	~Application();
@@ -15,14 +15,33 @@ public:
 	void Exit();
 	static bool IsKeyPressed(unsigned short key);
 
-	static const int _windowWidth = 800;
-    static const int _windowHeight = 600;
+	static const unsigned char FPS = 60; // FPS of this game
+	static const unsigned int frameTime = 1000 / FPS; // time for each frame
 
-    static const unsigned char FPS = 60; // FPS of this game
-    static const unsigned int frameTime = 1000 / FPS; // time for each frame
-    
+	// Returns the current window Width
+	static int windowWidth() { return _windowWidth; };
+
+	// Returns the current window Height
+	static int windowHeight() { return _windowHeight; };
+
+	// Is the application currently in Fullscreen?
+	static bool IsFullScreenMode() { return _isFullScreen; };
+
+	// Switch between Fullscreen and Window mode
+	static void ToggleFullscreen();
+
+	// Was window resized recently?
+	static bool IsWindowValid() { return _isValid; };
+
+	// Flag window as clean
+	static void ValidateWindow() { _isValid = true; };
+
 private:
+	static int _windowWidth;
+	static int _windowHeight;
 
+	static bool _isFullScreen;
+	static bool _isValid;
 
 };
 
